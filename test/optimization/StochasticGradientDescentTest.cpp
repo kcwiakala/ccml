c83e6f86d188269ab45ff6fa5cc10a0ce99fa2c4
kcwiakala <konrad.cwiakala@gmail.com>
2018-01-15 22:23:39 +0100

    Updating cmake

--- test/optimization/StochasticGradientDescentTest.cpp
+++ test/optimization/StochasticGradientDescentTest.cpp
@@ -24,12 +24,12 @@ TEST_F(StochasticGradientDescentTest, simple)
   net.push(l2);
   // net.push(l3);
 
-  l1->init(Initializer::uniform(-0.3, -0.5), Initializer::uniform(-0.2, -0.5));
-  l2->init(Initializer::uniform(-0.2, -0.5), Initializer::uniform(-0.2, -0.5));
+  l1->init(Initializer::uniform(-0.3, -0.5), Initializer::uniform(-0.3, -0.5));
+  l2->init(Initializer::uniform(-0.3, -0.5), Initializer::uniform(-0.3, -0.5));
   // l3->init(Initializer::uniform(-0.2, -0.5), Initializer::uniform(-0.2, -0.5));
 
-  _optimizer = std::make_unique<StochasticGradientDescent>(net, Loss::quadratic(), 0.5);
-  //_optimizer = std::make_unique<Momentum>(net, Loss::quadratic(), 0.5, 0.8);
+  //_optimizer = std::make_unique<StochasticGradientDescent>(net, Loss::quadratic(), 0.5);
+  _optimizer = std::make_unique<Momentum>(net, Loss::quadratic(), 0.5, 0.8);
 
   array_t aux;
 
@@ -42,11 +42,11 @@ TEST_F(StochasticGradientDescentTest, simple)
     {{0,0}, {0}}
   };
   
-  std::cout << "Loss before training: " << loss->compute(net, xorSamples) << std::endl;
+  // std::cout << "Loss before training: " << loss->compute(net, xorSamples) << std::endl;
   const bool success = _optimizer->train(xorSamples, 100000, 0.001);
   EXPECT_TRUE(success);
 
-  std::cout << "Loss after training: " << loss->compute(net, xorSamples) << std::endl;
+  // std::cout << "Loss after training: " << loss->compute(net, xorSamples) << std::endl;
 
   EXPECT_GT(net.output(xorSamples[0].input)[0], 0.5);
   EXPECT_GT(net.output({0, 1})[0], 0.5);
